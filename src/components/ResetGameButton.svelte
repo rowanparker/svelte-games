93383c5f4d85b028e0c34a7f0196a9c770e7a569
Rowan Parker <rowanparker@gmail.com>
2021-07-29 10:37:35 +1000

    Extract reset game button into component.

---
+++ src/components/ResetGameButton.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import {createEventDispatcher} from "svelte";
+    const dispatch = createEventDispatcher();
+</script>
+
+<div class="reset">
+    <button type="button" on:click={() => dispatch('resetGame')}>Reset Game</button>
+</div>
+
+<style>
+    .reset {
+        padding: 10px;
+        text-align: center;
+    }
+    .reset button {
+        border: 1px solid #cec4b3;
+        background-color: #dcd4c6;
+        font-size: 20px;
+        color: #5e503a;
+        padding: 15px 45px;
+        cursor: pointer;
+    }
+</style>
--- src/games/TicTacToe.svelte
+++ src/games/TicTacToe.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import ResetGameButton from "../components/ResetGameButton.svelte";
     let board: Array<number>;
     let gameState: number;
 
@@ -127,16 +128,11 @@
     {/each}
 </div>
 
-<div class="reset">
-    <button type="button" on:click={resetGame}>Reset Game</button>
-</div>
+<ResetGameButton on:resetGame={resetGame} />
 
 <style>
-    .status, .board, .reset {
-        /*margin: 0 auto;*/
+    .status, .board {
         text-align: center;
-        /*width: 100%;*/
-        /*padding: 10px;*/
     }
     .status {
         text-align: center;
@@ -172,13 +168,5 @@
     .player2 {
         background-color: #6b705c;
         color: #b7b7a4
-    }
-    .reset button {
-        border: 1px solid #cec4b3;
-        background-color: #dcd4c6;
-        font-size: 20px;
-        color: #5e503a;
-        padding: 15px 45px;
-        cursor: pointer;
     }
 </style>
